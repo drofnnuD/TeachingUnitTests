58215e04b701e15e9d4ae2454b0fcc7fa8cebd13
drofnnuD <Matt.Dunn2@googlemail.com>
2015-07-03 10:25:37 +0100

    inital commit

---
+++ src/com/example/anothertestproject/test/QuickExampleTest.java
@@ -0,0 +1,34 @@
+package com.example.anothertestproject.test;
+
+import com.example.anothertestproject.MainActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class QuickExampleTest extends ActivityInstrumentationTestCase2<MainActivity>{
+
+	private MainActivity myActivity;
+	
+	public QuickExampleTest(){
+		super(MainActivity.class);
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		myActivity = getActivity();
+	}
+
+	public void testPreconditions(){
+		assertNotNull("MainActivity not null", myActivity);
+	}
+	
+	public void testActivityLoadsProperly(){
+		assertTrue(myActivity.getTitle().toString().equals("AnotherTestProject"));
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+}
